d89ba94fe028ef0b7f0a2d8a1ad98e7a63bb4543
Danielle Hoo <daniellem.hoo@gmail.com>
2020-12-11 20:20:46 -0500

    dict challenge

---
+++ week 3/dict_challenge.py
@@ -0,0 +1,12 @@
+user = {"name": "Rex",
+        "height": "7'",
+        "shoe_size": 9,
+        "hair": "green",
+        "eye_color": "brown"}
+
+
+print(f"{user['name']}")
+print(f"{user['height']}")
+print(f"{user['shoe_size']}")
+print(f"{user['hair']}")
+print(f"{user['eye_color']}")
---
+++ week 3/dictionaries.py
@@ -0,0 +1,10 @@
+stock = ["Microsoft," "MSFT", "NASDAQ", "108.25", "106.23"]
+stock[1]
+
+# Dictionaries let you label values using strings
+stock = {"name": "Microsoft", 
+        "ticker": "MSFT", 
+        "index": "NASDAQ"}
+
+print(stock["name"])
+print(f"{stock['name']}")
